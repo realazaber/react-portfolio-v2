a93b86b48e50a07d41f2f1b115fef6fbbe0d14e4
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-30 21:58:01 +1100

    contact done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,31 +6,43 @@ function Contact() {
     return (
         <div className="container">            
             <div id="contact">
-                <h1 className="sectionTitleSecondary">
+                <h1 className="sectionTitlePrimary">
                     Contact me
                 </h1>
                 <Zoom>
                     <form id="contactForm" action="https://formspree.io/f/xqkwvwdg" method="post">
-                        <label htmlFor="name">Name *</label>
-                        <br />
-                        <input className="inputField" id="name" type="text" name="Name" required />
-                        <br />
-                        <label htmlFor="email">Email *</label>
-                        <br />
-                        <input className="inputField" id="email" type="email" name="Email" required />
-                        <br />
-                        <label htmlFor="phone">Phone </label>
-                        <br />
-                        <input className="inputField" id="phone" type="number" name="Number" required />
-                        <br />
-                        <label htmlFor="message">Message *</label>
-                        <br />
-                        <textarea className="inputField" id="message" cols="30" rows="6" name="Message"></textarea>
-                        <br />
-                        <button className="inputField" id="btnSubmit" type="submit">
-                            Submit
-                        </button>    
+                    
+                        <div id="inputFields">
+                            <div>
+                                <div className="inputSection">                                                    
+                                    <label htmlFor="name">Name *</label>
+                                    <input id="name" type="text" name="Name" required />                            
+                                </div>
+                            
+                                <div className="inputSection">                                                    
+                                    <label htmlFor="email">Email *</label>
+                                    <input id="email" type="email" name="Email" required />                            
+                                </div>
+                                
+                                <div className="inputSection">                                                    
+                                    <label htmlFor="phone">Phone </label>                            
+                                    <input id="phone" type="number" name="Number" required />                            
+                                </div>
+                            </div>
+                            
+                            <div>
+                                <div className="inputSection">                                            
+                                    <label htmlFor="message">Message *</label>
+                                    <textarea id="message" cols="30" rows="8" name="Message"></textarea>                            
+                                </div>
+                            </div>
+                        </div>
+
+                        <button id="btnSubmit" type="submit">
+                        Submit
+                    </button>                                   
                     </form>
+
                 </Zoom>
             </div>
         
