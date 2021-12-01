dedc6ea84ac2372ef449c15de430592cbd5ee1aa
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-12-01 15:44:35 +1100

    build

---
+++ build/precache-manifest.556561835009c4697d713ed8438482c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e544355d779c297b7d965be2386d518",
+    "url": "/react-portfolio-v2/index.html"
+  },
+  {
+    "revision": "a496ad5ca9ea97060d85",
+    "url": "/react-portfolio-v2/static/css/main.b982f5f0.chunk.css"
+  },
+  {
+    "revision": "9663240702c7253bd41b",
+    "url": "/react-portfolio-v2/static/js/2.05296501.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/react-portfolio-v2/static/js/2.05296501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a496ad5ca9ea97060d85",
+    "url": "/react-portfolio-v2/static/js/main.e1e36a34.chunk.js"
+  },
+  {
+    "revision": "24e503fc2fb5892aac5a",
+    "url": "/react-portfolio-v2/static/js/runtime-main.bc8ebd4c.js"
+  },
+  {
+    "revision": "a5e864f212cdf65048e9765e8c513f55",
+    "url": "/react-portfolio-v2/static/media/hero.a5e864f2.jpg"
+  },
+  {
+    "revision": "7bc440a22d454c115367a9e79802b871",
+    "url": "/react-portfolio-v2/static/media/logo.7bc440a2.png"
+  },
+  {
+    "revision": "179d289d22ce3cf3e6a5ac9ece3db87f",
+    "url": "/react-portfolio-v2/static/media/me.179d289d.png"
+  }
+]);
