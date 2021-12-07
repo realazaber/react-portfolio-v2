57752514aec7dabaed31496584b30c265c2068ae
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-12-08 10:58:12 +1100

    Updates

--- precache-manifest.7f878b0ebcf077f65d5c0f1e852a4add.js
+++ precache-manifest.7f878b0ebcf077f65d5c0f1e852a4add.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3871a5da03acd819903248c69714c008",
+    "revision": "5948d1175120b8d83790386d34d07a85",
     "url": "/react-portfolio-v2/index.html"
   },
   {
-    "revision": "5f70b2963794e15d0475",
-    "url": "/react-portfolio-v2/static/css/main.65bd36fa.chunk.css"
+    "revision": "93d380caaf63d86542a7",
+    "url": "/react-portfolio-v2/static/css/main.d87d8663.chunk.css"
   },
   {
     "revision": "9663240702c7253bd41b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio-v2/static/js/2.05296501.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5f70b2963794e15d0475",
-    "url": "/react-portfolio-v2/static/js/main.b7692ce3.chunk.js"
+    "revision": "93d380caaf63d86542a7",
+    "url": "/react-portfolio-v2/static/js/main.5f99d86b.chunk.js"
   },
   {
     "revision": "24e503fc2fb5892aac5a",
